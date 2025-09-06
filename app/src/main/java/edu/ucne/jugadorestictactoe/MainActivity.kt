309e2e05ae79cbe578c18c4e978674a693cccea4
erick <erickestradarosario@gmail.com>
2025-09-05 22:48:03 -0400

    Corrige Imports

--- app/src/main/java/edu/ucne/jugadorestictactoe/MainActivity.kt
+++ app/src/main/java/edu/ucne/jugadorestictactoe/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -11,7 +12,9 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.rememberNavController
 import dagger.hilt.android.AndroidEntryPoint
+import edu.ucne.jugadorestictactoe.presentation.Navigation.HostNavigation
 import edu.ucne.jugadorestictactoe.ui.theme.JugadoresTicTacToeTheme
 
 @AndroidEntryPoint
@@ -21,29 +24,19 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             JugadoresTicTacToeTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                val nav = rememberNavController()
+                Scaffold(
+                    modifier = Modifier.fillMaxSize()
+                ) { innerPadding ->
+                    Box(
                         modifier = Modifier.padding(innerPadding)
-                    )
+                    ) {
+                        HostNavigation(nav)
+                    }
+
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    JugadoresTicTacToeTheme {
-        Greeting("Android")
-    }
-}
