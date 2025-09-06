f2b3178cb7eb8be1679b2351bc260a8124957119
erick <erickestradarosario@gmail.com>
2025-09-05 22:43:28 -0400

    Crea JugadorDao

---
+++ app/src/main/java/edu/ucne/jugadorestictactoe/data/local/Dao/JugadorDao.kt
@@ -0,0 +1,28 @@
+package edu.ucne.jugadorestictactoe.data.local.Dao
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Query
+import androidx.room.Upsert
+import edu.ucne.jugadorestictactoe.data.local.Entities.JugadorEntity
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface JugadorDao{
+    @Upsert
+    suspend fun save(jugador: JugadorEntity)
+
+    @Query("""
+            SELECT *
+            FROM Jugadores
+            WHERE jugadorId =:id
+            Limit 1
+    """)
+    suspend fun find(id: Int): JugadorEntity?
+
+    @Delete
+    suspend fun delete(jugador: JugadorEntity)
+
+    @Query("SELECT * FROM Jugadores")
+    fun getAll(): Flow<List<JugadorEntity>>
+}
