6352840e06da50ff857c3c3b647f6ee03cc3ab3a
erick <erickestradarosario@gmail.com>
2025-09-05 22:45:42 -0400

    Crea caso de uso guardar

---
+++ app/src/main/java/edu/ucne/jugadorestictactoe/domain/useCase/GuardarJugadorUseCase.kt
@@ -0,0 +1,19 @@
+package edu.ucne.jugadorestictactoe.domain.useCase
+
+import edu.ucne.jugadorestictactoe.domain.model.Jugador
+import edu.ucne.jugadorestictactoe.domain.repository.JugadorRepository
+
+
+class GuardarJugadorUseCase(
+    private val repository: JugadorRepository,
+    private val validarJugador: ValidarJugadorUseCase
+) {
+    suspend operator fun invoke(jugador: Jugador): Result<Boolean> {
+
+        val validacion = validarJugador(jugador)
+        if (validacion.isFailure) return Result.failure(validacion.exceptionOrNull()!!)
+
+        val result = repository.save(jugador)
+        return Result.success(result)
+    }
+}
