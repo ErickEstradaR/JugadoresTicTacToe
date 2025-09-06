bae27abf7fda3c8c05428af8c6fa806c75516486
erick <erickestradarosario@gmail.com>
2025-09-05 22:46:35 -0400

    Crea JugadorUiState

---
+++ app/src/main/java/edu/ucne/jugadorestictactoe/presentation/Jugador/JugadorUiState.kt
@@ -0,0 +1,12 @@
+package edu.ucne.jugadorestictactoe.presentation.Jugador
+
+import edu.ucne.jugadorestictactoe.data.local.Entities.JugadorEntity
+import edu.ucne.jugadorestictactoe.domain.model.Jugador
+
+data class JugadorUiState (
+    val jugadorId: Int? = null,
+    val nombres: String = "",
+    val partidas: Int = 0,
+    val errorMessage: String? = null,
+    val jugadores: List<Jugador> = emptyList()
+)
