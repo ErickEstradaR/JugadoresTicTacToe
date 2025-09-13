7eaee94034f6f9bda37f9545715fa74e2a81894b
erick <erickestradarosario@gmail.com>
2025-09-13 14:34:02 -0400

    Crea NavigationDrawer

---
+++ app/src/main/java/edu/ucne/jugadorestictactoe/presentation/Navigation/MainScreen.kt
@@ -0,0 +1,80 @@
+package edu.ucne.jugadorestictactoe.presentation.Navigation
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.filled.PlayArrow
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.currentBackStackEntryAsState
+import kotlinx.coroutines.launch
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MainScreen(navController: NavHostController) {
+    val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
+    val scope = rememberCoroutineScope()
+
+    val navBackStackEntry by navController.currentBackStackEntryAsState()
+    val currentRoute = navBackStackEntry?.destination?.route
+
+    ModalNavigationDrawer(
+        drawerState = drawerState,
+        drawerContent = {
+            ModalDrawerSheet {
+                Text(
+                    "Menú",
+                    style = MaterialTheme.typography.titleMedium,
+                    modifier = Modifier.padding(16.dp)
+                )
+
+                NavigationDrawerItem(
+                    label = { Text("Jugadores") },
+                    selected = currentRoute == Screen.List::class.qualifiedName,
+                    onClick = {
+                        scope.launch { drawerState.close() }
+                        navController.navigate(Screen.List)
+                    },
+                    icon = { Icon(Icons.Default.Person, contentDescription = "Jugadores") }
+                )
+
+                NavigationDrawerItem(
+                    label = { Text("Partidas") },
+                    selected = currentRoute == Screen.PartidaList::class.qualifiedName,
+                    onClick = {
+                        scope.launch { drawerState.close() }
+                        navController.navigate(Screen.PartidaList)
+                              },
+                    icon = { Icon(Icons.Default.PlayArrow, contentDescription = "Partidas") }
+                )
+            }
+        }
+    ) {
+        Scaffold(
+            topBar = {
+                SmallTopAppBar(
+                    title = { Text("Aplicada 2") },
+                    navigationIcon = {
+                        IconButton(onClick = {
+                            scope.launch {
+                                if (drawerState.isClosed) drawerState.open() else drawerState.close()
+                            }
+                        }) {
+                            Icon(Icons.Default.Menu, contentDescription = "Menú")
+                        }
+                    }
+                )
+            }
+        ) { paddingValues ->
+            HostNavigation(
+                navHostController = navController,
+                modifier = Modifier.padding(paddingValues))
+        }
+    }
+}
