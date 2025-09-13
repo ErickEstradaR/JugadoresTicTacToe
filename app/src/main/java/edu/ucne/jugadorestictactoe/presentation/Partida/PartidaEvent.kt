d244bcb9f8d3c82a38f4976a506510af4dbdf6da
erick <erickestradarosario@gmail.com>
2025-09-12 20:31:28 -0400

    Corrige nombres

--- app/src/main/java/edu/ucne/jugadorestictactoe/presentation/Partida/PartidaEvent.kt
+++ app/src/main/java/edu/ucne/jugadorestictactoe/presentation/Partida/PartidaEvent.kt
@@ -1,16 +1,17 @@
 package edu.ucne.jugadorestictactoe.presentation.Partida
 
+import edu.ucne.jugadorestictactoe.presentation.Jugador.JugadorEvent
 
 
 sealed interface PartidaEvent {
-    data class IdChange(val id: Int?) : PartidaEvent
-    data class FechaChange(val fecha: String) : PartidaEvent
-    data class Jugador1Change(val jugador1: Int) : PartidaEvent
-    data class Jugador2Change(val jugador2: Int) : PartidaEvent
-    data class GanadorChange(val ganadorId: Int?) : PartidaEvent
-    data class EsFinalizadaChange(val esFinalizada: Boolean) : PartidaEvent
+    data class partidaChange(val partidaId: Int) : PartidaEvent
+    data class fechaChange(val fecha: String) : PartidaEvent
+    data class jugador1Change(val jugador1: Int) : PartidaEvent
+    data class jugador2Change(val jugador2: Int) : PartidaEvent
+    data class ganadorChange(val ganadorId: Int?) : PartidaEvent
+    data class esFinalizadaChange(val esFinalizada: Boolean) : PartidaEvent
 
-    data object Save : PartidaEvent
-    data object Delete : PartidaEvent
-    data object New : PartidaEvent
+    data object save : PartidaEvent
+    data object delete : PartidaEvent
+    data object new : PartidaEvent
 }
