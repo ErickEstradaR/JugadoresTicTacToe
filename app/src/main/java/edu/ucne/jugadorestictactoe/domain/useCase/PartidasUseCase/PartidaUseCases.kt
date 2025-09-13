67a220349b877881204927fe36fdf868c72a9ff0
erick <erickestradarosario@gmail.com>
2025-09-13 14:56:22 -0400

    Agrega ValidarPartidaUseCase

--- app/src/main/java/edu/ucne/jugadorestictactoe/domain/useCase/PartidasUseCase/PartidaUseCases.kt
+++ app/src/main/java/edu/ucne/jugadorestictactoe/domain/useCase/PartidasUseCase/PartidaUseCases.kt
@@ -4,5 +4,6 @@ data class PartidaUseCases (
     val eliminarPartida : EliminarPartidaUseCase,
     val guardarPartida : GuardarPartidaUseCase,
     val obtenerPartida : ObtenerPartidaUseCase,
-    val obtenerPartidas : ObtenerPartidasUseCase
+    val obtenerPartidas : ObtenerPartidasUseCase,
+    val validarPartida : ValidarPartidaUseCase
 )
