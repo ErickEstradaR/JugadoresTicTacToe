5f6ded2de52b9e68a9adc3e2b037ec96ef091efb
erick <erickestradarosario@gmail.com>
2025-09-12 20:55:26 -0400

    Añade funcionalidades de partida

--- app/src/main/java/edu/ucne/jugadorestictactoe/di/AppModule.kt
+++ app/src/main/java/edu/ucne/jugadorestictactoe/di/AppModule.kt
@@ -8,15 +8,22 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
-import edu.ucne.jugadorestictactoe.data.local.Database.JugadorDb
-import edu.ucne.jugadorestictactoe.data.repository.JugadorRepositoryImpl
+import edu.ucne.jugadorestictactoe.data.Database.JugadorDb
+import edu.ucne.jugadorestictactoe.data.local.repository.JugadorRepositoryImpl
+import edu.ucne.jugadorestictactoe.data.local.repository.PartidaRepositoryImpl
 import edu.ucne.jugadorestictactoe.domain.repository.JugadorRepository
-import edu.ucne.jugadorestictactoe.domain.useCase.EliminarJugadorUseCase
-import edu.ucne.jugadorestictactoe.domain.useCase.GuardarJugadorUseCase
-import edu.ucne.jugadorestictactoe.domain.useCase.JugadorUseCases
-import edu.ucne.jugadorestictactoe.domain.useCase.ObtenerJugadorUseCase
-import edu.ucne.jugadorestictactoe.domain.useCase.ObtenerJugadoresUseCase
-import edu.ucne.jugadorestictactoe.domain.useCase.ValidarJugadorUseCase
+import edu.ucne.jugadorestictactoe.domain.repository.PartidaRepository
+import edu.ucne.jugadorestictactoe.domain.useCase.JugadoresUseCase.EliminarJugadorUseCase
+import edu.ucne.jugadorestictactoe.domain.useCase.JugadoresUseCase.GuardarJugadorUseCase
+import edu.ucne.jugadorestictactoe.domain.useCase.JugadoresUseCase.JugadorUseCases
+import edu.ucne.jugadorestictactoe.domain.useCase.JugadoresUseCase.ObtenerJugadorUseCase
+import edu.ucne.jugadorestictactoe.domain.useCase.JugadoresUseCase.ObtenerJugadoresUseCase
+import edu.ucne.jugadorestictactoe.domain.useCase.JugadoresUseCase.ValidarJugadorUseCase
+import edu.ucne.jugadorestictactoe.domain.useCase.PartidasUseCase.EliminarPartidaUseCase
+import edu.ucne.jugadorestictactoe.domain.useCase.PartidasUseCase.GuardarPartidaUseCase
+import edu.ucne.jugadorestictactoe.domain.useCase.PartidasUseCase.ObtenerPartidaUseCase
+import edu.ucne.jugadorestictactoe.domain.useCase.PartidasUseCase.ObtenerPartidasUseCase
+import edu.ucne.jugadorestictactoe.domain.useCase.PartidasUseCase.PartidaUseCases
 import javax.inject.Singleton
 
 @InstallIn(SingletonComponent::class)
@@ -25,7 +32,7 @@ object AppModule {
 
     @Provides
     @Singleton
-    fun providesTareaDb(@ApplicationContext appContext: Context) =
+    fun providesJugadorDb(@ApplicationContext appContext: Context) =
         Room.databaseBuilder(
             appContext,
             JugadorDb::class.java,
@@ -33,8 +40,11 @@ object AppModule {
         ).fallbackToDestructiveMigration()
             .build()
 
+
     @Provides
     fun providesJugadorDao(jugadorDb: JugadorDb) = jugadorDb.JugadorDao()
+    @Provides
+    fun providesPartidaDao(jugadorDb: JugadorDb)= jugadorDb.PartidaDao()
 
     @Provides
     fun provideJugadorUseCases(repository: JugadorRepository): JugadorUseCases {
@@ -46,6 +56,15 @@ object AppModule {
             obtenerJugador = ObtenerJugadorUseCase(repository),
             obtenerJugadores = ObtenerJugadoresUseCase(repository)
         )}
+    @Provides
+    fun providePartidaUseCases(repository: PartidaRepository): PartidaUseCases{
+        return PartidaUseCases (
+            guardarPartida = GuardarPartidaUseCase(repository),
+            eliminarPartida = EliminarPartidaUseCase(repository),
+            obtenerPartida = ObtenerPartidaUseCase(repository),
+            obtenerPartidas = ObtenerPartidasUseCase(repository)
+        )}
+
     @Module
     @InstallIn(SingletonComponent::class)
 
@@ -56,6 +75,12 @@ object AppModule {
         abstract fun bindJugadorRepository(
             impl: JugadorRepositoryImpl
         ): JugadorRepository
+
+        @Binds
+        @Singleton
+        abstract fun bindPartidaRepository(
+            impl: PartidaRepositoryImpl
+        ): PartidaRepository
     }
 }
 
