51df761450528b60fa05c6e558d91507f7f314bd
erick <erickestradarosario@gmail.com>
2025-09-13 14:37:15 -0400

    Añade validación de ganador

--- app/src/main/java/edu/ucne/jugadorestictactoe/domain/useCase/PartidasUseCase/ValidarPartidaUseCase.kt
+++ app/src/main/java/edu/ucne/jugadorestictactoe/domain/useCase/PartidasUseCase/ValidarPartidaUseCase.kt
@@ -4,10 +4,16 @@ import edu.ucne.jugadorestictactoe.domain.model.Partida
 
 class ValidarPartidaUseCase {
     operator fun invoke(partida: Partida): Result<Unit> {
-        return if (partida.jugador1 == partida.jugador2) {
-            Result.failure(IllegalArgumentException("Un jugador no puede jugar contra sí mismo"))
-        } else {
-            Result.success(Unit)
+        if (partida.jugador1 == partida.jugador2) {
+            return Result.failure(IllegalArgumentException("Un jugador no puede jugar contra sí mismo"))
         }
+        if (partida.esFinalizada) {
+            val ganador = partida.ganadorId
+            if (ganador != partida.jugador1 && ganador != partida.jugador2) {
+                return Result.failure(IllegalArgumentException("El ganador debe ser uno de los jugadores de la partida"))
+            }
+        }
+
+        return Result.success(Unit)
     }
 }
