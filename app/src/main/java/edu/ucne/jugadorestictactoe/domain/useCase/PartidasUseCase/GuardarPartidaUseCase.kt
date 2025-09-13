dd023c8d860c5c6d658841ac14ef6fb15cb314aa
erick <erickestradarosario@gmail.com>
2025-09-13 14:37:23 -0400

    Añade validación antes de guardar

--- app/src/main/java/edu/ucne/jugadorestictactoe/domain/useCase/PartidasUseCase/GuardarPartidaUseCase.kt
+++ app/src/main/java/edu/ucne/jugadorestictactoe/domain/useCase/PartidasUseCase/GuardarPartidaUseCase.kt
@@ -3,11 +3,18 @@ package edu.ucne.jugadorestictactoe.domain.useCase.PartidasUseCase
 import edu.ucne.jugadorestictactoe.domain.model.Partida
 import edu.ucne.jugadorestictactoe.domain.repository.PartidaRepository
 
-class GuardarPartidaUseCase (
+class GuardarPartidaUseCase(
     private val repository: PartidaRepository,
+    private val validarPartidaUseCase: ValidarPartidaUseCase
 ) {
     suspend operator fun invoke(partida: Partida): Result<Boolean> {
+        val validacion = validarPartidaUseCase(partida)
+        if (validacion.isFailure) {
+            return Result.failure(validacion.exceptionOrNull()!!)
+        }
+
         val result = repository.save(partida)
         return Result.success(result)
     }
 }
+
