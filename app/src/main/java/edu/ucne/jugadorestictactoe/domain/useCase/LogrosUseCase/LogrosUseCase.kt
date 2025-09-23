605930a28ed7bcecc697ba3291312a7ff32ed552
erick <erickestradarosario@gmail.com>
2025-09-23 13:31:32 -0400

    Crea LogrosUseCases

---
+++ app/src/main/java/edu/ucne/jugadorestictactoe/domain/useCase/LogrosUseCase/LogrosUseCase.kt
@@ -0,0 +1,10 @@
+package edu.ucne.jugadorestictactoe.domain.useCase.LogrosUseCase
+
+
+
+data class LogrosUseCases (
+    val eliminarLogro : EliminarLogroUseCase,
+    val guardarLogro : GuardarLogroUseCase,
+    val obtenerLogro : ObtenerLogroUseCase,
+    val obtenerLogros : ObtenerLogrosUseCase,
+)
