d8bd982e8ea526237e7d0bc2e767063fa73586c2
erick <erickestradarosario@gmail.com>
2025-09-23 13:23:00 -0400

    Crea interfaz LogroRepository

--- app/src/main/java/edu/ucne/jugadorestictactoe/domain/repository/LogroRepository.kt
+++ app/src/main/java/edu/ucne/jugadorestictactoe/domain/repository/LogroRepository.kt
@@ -1,4 +1,13 @@
 package edu.ucne.jugadorestictactoe.domain.repository
 
-class LogroRepository {
+import edu.ucne.jugadorestictactoe.domain.model.Logro
+import edu.ucne.jugadorestictactoe.domain.model.Partida
+import kotlinx.coroutines.flow.Flow
+
+interface LogroRepository {
+    suspend fun save(logro: Logro): Boolean
+    suspend fun find(id: Int): Logro?
+    suspend fun delete(logro: Logro)
+    suspend fun getAll(): List<Logro>
+    fun getAllFlow(): Flow<List<Logro>>
 }
