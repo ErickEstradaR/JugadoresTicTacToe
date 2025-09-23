4fb20019c97e2532d54190ec6c75831ae1f9d668
erick <erickestradarosario@gmail.com>
2025-09-23 13:36:13 -0400

    Crea LogroUiState

---
+++ app/src/main/java/edu/ucne/jugadorestictactoe/presentation/Logro/LogroUiState.kt
@@ -0,0 +1,15 @@
+package edu.ucne.jugadorestictactoe.presentation.Logro
+
+import edu.ucne.jugadorestictactoe.domain.model.Logro
+
+data class LogroUiState (
+    val LogroId: Int? = null,
+    val nombre: String = "",
+    val descripcion: String = "",
+    val errorMessage: String? = null,
+    val logros: List<Logro> = emptyList()
+) {
+    companion object {
+        fun default() = LogroUiState()
+    }
+}
