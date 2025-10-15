ba9ba822bea5f3bb5e04b2272d552935e9558c1e
Erick_Estrada <erickestradarosario@gmail.com>
2025-10-15 13:53:45 -0300

    Elimina imports innecesarios en MainActivity y ObtenerTecnicoUseCase

--- app/src/main/java/edu/ucne/jugadorestictactoe/MainActivity.kt
+++ app/src/main/java/edu/ucne/jugadorestictactoe/MainActivity.kt
@@ -4,14 +4,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.ui.Modifier
 import androidx.navigation.compose.rememberNavController
 import dagger.hilt.android.AndroidEntryPoint
-import edu.ucne.jugadorestictactoe.presentation.Navigation.HostNavigation
 import edu.ucne.jugadorestictactoe.presentation.Navigation.MainScreen
 import edu.ucne.jugadorestictactoe.ui.theme.JugadoresTicTacToeTheme
 
--- app/src/main/java/edu/ucne/jugadorestictactoe/domain/useCase/TecnicoUseCase/ObtenerTecnicoUseCase.kt
+++ app/src/main/java/edu/ucne/jugadorestictactoe/domain/useCase/TecnicoUseCase/ObtenerTecnicoUseCase.kt
@@ -2,7 +2,7 @@ package edu.ucne.jugadorestictactoe.domain.useCase.TecnicoUseCase
 
 import edu.ucne.jugadorestictactoe.domain.model.Tecnico
 import edu.ucne.jugadorestictactoe.domain.repository.TecnicoRepository
-import kotlinx.coroutines.flow.Flow
+
 
 
 class ObtenerTecnicoUseCase(
