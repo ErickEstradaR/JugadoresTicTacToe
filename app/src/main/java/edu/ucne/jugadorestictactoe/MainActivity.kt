a728a447dc485f5b426e2de18bcf1da3c08c2959
Erick_Estrada <erickestradarosario@gmail.com>
2025-10-15 01:03:32 -0300

    Refactoriza MainActivity para usar Scaffold
    Además, la llamada a `MainScreen` se ha actualizado para pasar el `navController` como un argumento con nombre, mejorando la legibilidad del código.

--- app/src/main/java/edu/ucne/jugadorestictactoe/MainActivity.kt
+++ app/src/main/java/edu/ucne/jugadorestictactoe/MainActivity.kt
@@ -4,8 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.ui.Modifier
 import androidx.navigation.compose.rememberNavController
 import dagger.hilt.android.AndroidEntryPoint
+import edu.ucne.jugadorestictactoe.presentation.Navigation.HostNavigation
 import edu.ucne.jugadorestictactoe.presentation.Navigation.MainScreen
 import edu.ucne.jugadorestictactoe.ui.theme.JugadoresTicTacToeTheme
 
@@ -17,9 +23,8 @@ class MainActivity : ComponentActivity() {
         setContent {
             JugadoresTicTacToeTheme {
                 val navController = rememberNavController()
-                MainScreen(navController)
+                MainScreen(navController = navController)
             }
         }
     }
 }
-
