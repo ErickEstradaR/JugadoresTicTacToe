0e4ec3138de761a0f8e0f30a7c433b5296ab7387
Erick_Estrada <erickestradarosario@gmail.com>
2025-10-16 14:07:57 -0300

    Elimina import innecesario en MovimientoDto

--- app/src/main/java/edu/ucne/jugadorestictactoe/data/remote/dto/partida/MovimientoDto.kt
+++ app/src/main/java/edu/ucne/jugadorestictactoe/data/remote/dto/partida/MovimientoDto.kt
@@ -1,12 +1,10 @@
 package edu.ucne.jugadorestictactoe.data.remote.dto.partida
 
-import com.squareup.moshi.Json
-
 data class MovimientoDto (
-val PartidaId : Int,
-val Jugador : String = "X",
-val PosicionFila : Int,
-val PosicionColumna : Int
+    val PartidaId : Int,
+    val Jugador : String = "X",
+    val PosicionFila : Int,
+    val PosicionColumna : Int
 )
 
 
