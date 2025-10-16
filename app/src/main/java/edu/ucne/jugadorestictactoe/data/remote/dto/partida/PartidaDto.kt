ad6e5ac590b690f1dc896a9c59a86ba3ab5f2638
Erick_Estrada <erickestradarosario@gmail.com>
2025-10-16 16:00:16 -0300

    Actualiza PartidaDto con nuevos campos y tipos nulables

--- app/src/main/java/edu/ucne/jugadorestictactoe/data/remote/dto/partida/PartidaDto.kt
+++ app/src/main/java/edu/ucne/jugadorestictactoe/data/remote/dto/partida/PartidaDto.kt
@@ -1,7 +1,22 @@
 package edu.ucne.jugadorestictactoe.data.remote.dto.partida
 
 data class PartidaDto (
-    val PartidaId : Int,
-    val Jugador1Id : Int,
-    val Jugador2Id : Int?,
+    val PartidaId: Int?,
+    val Jugador1Id: Int?,
+    val Jugador2Id: Int?,
+
+    val EstadoPartida: String?,
+    val GanadorId: Int?,
+    val TurnoJugadorId: Int?,
+    val EstadoTablero: String?,
+
+    val FechaInicio: String?,
+    val FechaFin: String?,
+
+    val Jugador1: String?,
+    val Jugador2: String?,
+    val Ganador: String?,
+    val TurnoJugador: String?,
+
+    val Movimientos: List<MovimientoDto>?
 )
