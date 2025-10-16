7ff833034e88fa3c7822c1097166df3793e326c4
Erick_Estrada <erickestradarosario@gmail.com>
2025-10-16 14:08:34 -0300

    Crea implementación de PartidaApiRepository

---
+++ app/src/main/java/edu/ucne/jugadorestictactoe/data/local/repository/PartidaApiRepositoryImpl.kt
@@ -0,0 +1,30 @@
+package edu.ucne.jugadorestictactoe.data.local.repository
+
+import edu.ucne.jugadorestictactoe.data.local.mappers.toDomain
+import edu.ucne.jugadorestictactoe.data.remote.dto.partida.PartidaDto
+import edu.ucne.jugadorestictactoe.data.remote.dto.partidaApi.PartidaApiService
+import edu.ucne.jugadorestictactoe.domain.model.PartidaApi
+import edu.ucne.jugadorestictactoe.domain.repository.PartidaApiRepository
+import javax.inject.Inject
+
+class PartidaApiRepositoryImpl @Inject constructor(
+        private val api: PartidaApiService
+    ) : PartidaApiRepository {
+
+        override suspend fun getPartidas(): List<PartidaApi> {
+            return api.getPartidas().map { it.toDomain() }
+        }
+
+        override suspend fun getPartida(partidaId: Int): PartidaApi {
+            return api.getPartida(partidaId).toDomain()
+        }
+
+        override suspend fun createPartida(player1Id: Int): PartidaApi {
+            val newPartidaDto = PartidaDto(
+                PartidaId = 0,
+                Jugador1Id = player1Id,
+                Jugador2Id = null
+            )
+            return api.createPartida(newPartidaDto).toDomain()
+        }
+    }
