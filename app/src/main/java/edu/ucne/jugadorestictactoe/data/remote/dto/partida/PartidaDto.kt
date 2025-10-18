d034f9de476fafc7243b5fb435f214dbdda0d435
Erick_Estrada <erickestradarosario@gmail.com>
2025-10-17 22:54:27 -0300

    Modifica PartidaDto para que PartidaId y Jugador1Id no sean nulos

--- app/src/main/java/edu/ucne/jugadorestictactoe/data/remote/dto/partida/PartidaDto.kt
+++ app/src/main/java/edu/ucne/jugadorestictactoe/data/remote/dto/partida/PartidaDto.kt
@@ -1,8 +1,8 @@
 package edu.ucne.jugadorestictactoe.data.remote.dto.partida
 
 data class PartidaDto (
-    val PartidaId: Int?,
-    val Jugador1Id: Int?,
+    val PartidaId: Int,
+    val Jugador1Id: Int,
     val Jugador2Id: Int?,
 
     val EstadoPartida: String?,
