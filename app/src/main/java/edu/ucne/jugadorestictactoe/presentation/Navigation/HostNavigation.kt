6bfb5477a73efbb0292d2d8c0348ef9d144f0baf
Erick_Estrada <erickestradarosario@gmail.com>
2025-10-17 22:56:45 -0300

    Refactoriza navegación para usar JugadorApiScreen y JugadorApiListScreen

--- app/src/main/java/edu/ucne/jugadorestictactoe/presentation/Navigation/HostNavigation.kt
+++ app/src/main/java/edu/ucne/jugadorestictactoe/presentation/Navigation/HostNavigation.kt
@@ -9,8 +9,6 @@ import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.toRoute
-import edu.ucne.jugadorestictactoe.presentation.Jugador.JugadorListScreen
-import edu.ucne.jugadorestictactoe.presentation.Jugador.JugadorScreen
 import edu.ucne.jugadorestictactoe.presentation.Logro.LogroScreen
 import edu.ucne.jugadorestictactoe.presentation.Logro.LogrolistScreen
 import edu.ucne.jugadorestictactoe.presentation.Partida.PartidaListScreen
@@ -20,6 +18,8 @@ import edu.ucne.jugadorestictactoe.presentation.Tecnico.TecnicoScreen
 import edu.ucne.jugadorestictactoe.presentation.tictactoe.GameViewModel
 import edu.ucne.jugadorestictactoe.presentation.tictactoe.PlayerSelectionScreen
 import edu.ucne.jugadorestictactoe.presentation.tictactoe.TicTacToeScreen
+import edu.ucne.jugadorestictactoe.presentation.tictactoe.jugador.JugadorApiListScreen
+import edu.ucne.jugadorestictactoe.presentation.tictactoe.jugador.JugadorApiScreen
 
 @Composable
 fun HostNavigation(
@@ -28,16 +28,16 @@ fun HostNavigation(
     ) {
     NavHost(
         navController = navHostController,
-        startDestination = Screen.List,
+        startDestination = Screen.JugadorApiList,
         modifier = modifier
     ) {
-        composable<Screen.List> {
-            JugadorListScreen(
-                goToJugadores = { id ->
-                    navHostController.navigate(Screen.Jugador(id))
+        composable<Screen.JugadorApiList> {
+            JugadorApiListScreen (
+                goToJugador = { id ->
+                    navHostController.navigate(Screen.JugadorApi(id))
                 },
                 createJugador = {
-                    navHostController.navigate(Screen.Jugador(null))
+                    navHostController.navigate(Screen.JugadorApi(null))
                 }
             )
         }
@@ -53,9 +53,9 @@ fun HostNavigation(
             )
         }
 
-        composable<Screen.Jugador> { backStack ->
-            val jugadorId = backStack.toRoute<Screen.Jugador>().Id
-            JugadorScreen(
+        composable<Screen.JugadorApi> { backStack ->
+            val jugadorId = backStack.toRoute<Screen.JugadorApi>().id
+            JugadorApiScreen(
                 jugadorId = jugadorId ?: 0,
                 goback = {navHostController.popBackStack()}
             )
@@ -64,9 +64,11 @@ fun HostNavigation(
         composable<Screen.GameScreen> { backStack ->
             val viewModel: GameViewModel = hiltViewModel()
             val state by viewModel.state.collectAsStateWithLifecycle()
+
             PlayerSelectionScreen(
-                selectedPlayer = state.playerSelection,
-                onPlayerSelected = viewModel::selectPlayer,
+                jugadorX = state.selectedJugador,
+                onJugadorXSelected = viewModel::setJugadorX,
+
                 onStartGame = viewModel::startGame
             )
         }
