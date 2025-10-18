93a932a39174529cddc5826696401af9cd9300fa
Erick_Estrada <erickestradarosario@gmail.com>
2025-10-17 22:55:31 -0300

    Actualiza JugadorDto para reflejar cambios en la API

--- app/src/main/java/edu/ucne/jugadorestictactoe/data/remote/dto/partida/JugadorDto.kt
+++ app/src/main/java/edu/ucne/jugadorestictactoe/data/remote/dto/partida/JugadorDto.kt
@@ -1,20 +1,18 @@
 package edu.ucne.jugadorestictactoe.data.remote.dto.partida
 
 data class JugadorDto(
-    val JugadorId: Int?,
-    val Nombres: String?,
-    val Email: String?,
+    val jugadorId: Int? = null,
+    val nombres: String,
+    val email: String,
+    val fechaCreacion: String?,
+    val victorias: Int?,
+    val derrotas: Int?,
+    val empates: Int?,
+
+    val partidasComoJugador1: List<PartidaDto>?,
+    val partidasComoJugador2: List<PartidaDto>?,
+    val partidasGanadas: List<PartidaDto>?,
 
 
-    val FechaCreacion: String?,
-    val Victorias: Int?,
-    val Derrotas: Int?,
-    val Empates: Int?,
-
-    val PartidasComoJugador1: List<PartidaDto>?,
-    val PartidasComoJugador2: List<PartidaDto>?,
-    val PartidasGanadas: List<PartidaDto>?,
-
-
-    val Movimientos: List<MovimientoDto>?
+    val movimientos: List<MovimientoDto>?
 )
