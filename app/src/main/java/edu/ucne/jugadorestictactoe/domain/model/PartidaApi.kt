0474bc7a18be439df7cb20b8fff57e731940ff67
Erick_Estrada <erickestradarosario@gmail.com>
2025-10-17 22:58:56 -0300

    Corrige PartidaApi para que no acepte valores nulos en PartidaId y Jugador1Id

--- app/src/main/java/edu/ucne/jugadorestictactoe/domain/model/PartidaApi.kt
+++ app/src/main/java/edu/ucne/jugadorestictactoe/domain/model/PartidaApi.kt
@@ -1,8 +1,8 @@
 package edu.ucne.jugadorestictactoe.domain.model
 
 data class PartidaApi (
-    val PartidaId: Int?,
-    val Jugador1Id: Int?,
+    val PartidaId: Int,
+    val Jugador1Id: Int,
     val Jugador2Id: Int?,
 
     val EstadoPartida: String?,
