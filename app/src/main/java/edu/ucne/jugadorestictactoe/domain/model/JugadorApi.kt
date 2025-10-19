209526704456586b37040e7724945d840a6154fd
Erick_Estrada <erickestradarosario@gmail.com>
2025-10-18 21:40:40 -0300

    Simplifica el modelo JugadorApi

--- app/src/main/java/edu/ucne/jugadorestictactoe/domain/model/JugadorApi.kt
+++ app/src/main/java/edu/ucne/jugadorestictactoe/domain/model/JugadorApi.kt
@@ -1,18 +1,6 @@
 package edu.ucne.jugadorestictactoe.domain.model
 
 data class JugadorApi (
-    val jugadorId: Int? = null,
     val nombres: String,
-    val email: String,
-    val fechaCreacion: String?,
-    val victorias: Int?,
-    val derrotas: Int?,
-    val empates: Int?,
-
-    val partidasComoJugador1: List<PartidaApi>?,
-    val partidasComoJugador2: List<PartidaApi>?,
-    val partidasGanadas: List<PartidaApi>?,
-
-
-    val movimientos: List<Movimiento>?
+    val email: String
 )
