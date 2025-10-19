c8b21a4f26e86e4833a59ea1ed013874e287176c
Erick_Estrada <erickestradarosario@gmail.com>
2025-10-18 21:40:00 -0300

    Actualiza modelo Movimiento para simplificar campos

--- app/src/main/java/edu/ucne/jugadorestictactoe/domain/model/Movimiento.kt
+++ app/src/main/java/edu/ucne/jugadorestictactoe/domain/model/Movimiento.kt
@@ -1,12 +1,8 @@
 package edu.ucne.jugadorestictactoe.domain.model
 
 data class Movimiento (
-    val MovimientoId: Int?,
-    val PartidaId: Int?,
-    val JugadorId: Int?,
-    val PosicionFila: Int?,
-    val PosicionColumna: Int?,
-    val FechaMovimiento: String?,
-    val Partida: String?,
-    val Jugador: String?
+    val partidaId: Int?,
+    val jugador : String,
+    val posicionFila: Int?,
+    val posicionColumna: Int?
 )
