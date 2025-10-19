600d070f7db269de47676032ee74fe8247959e09
Erick_Estrada <erickestradarosario@gmail.com>
2025-10-18 21:40:22 -0300

    Actualiza MovimientoDto para envío de datos

--- app/src/main/java/edu/ucne/jugadorestictactoe/data/remote/dto/partida/MovimientoDto.kt
+++ app/src/main/java/edu/ucne/jugadorestictactoe/data/remote/dto/partida/MovimientoDto.kt
@@ -1,14 +1,10 @@
 package edu.ucne.jugadorestictactoe.data.remote.dto.partida
 
 data class MovimientoDto(
-    val MovimientoId: Int?,
-    val PartidaId: Int?,
-    val JugadorId: Int?,
-    val PosicionFila: Int?,
-    val PosicionColumna: Int?,
-    val FechaMovimiento: String?,
-    val Partida: String?,
-    val Jugador: String?
+    val partidaId: Int?,
+    val jugador: String,
+    val posicionFila: Int?,
+    val posicionColumna: Int?
 )
 
 
