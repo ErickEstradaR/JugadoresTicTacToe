cc044fd513b376f616ca05c571583f1a0424b931
Erick_Estrada <erickestradarosario@gmail.com>
2025-10-18 21:40:30 -0300

    Simplifica JugadorDto para la partida

--- app/src/main/java/edu/ucne/jugadorestictactoe/data/remote/dto/partida/JugadorDto.kt
+++ app/src/main/java/edu/ucne/jugadorestictactoe/data/remote/dto/partida/JugadorDto.kt
@@ -1,18 +1,6 @@
 package edu.ucne.jugadorestictactoe.data.remote.dto.partida
 
 data class JugadorDto(
-    val jugadorId: Int? = null,
     val nombres: String,
     val email: String,
-    val fechaCreacion: String?,
-    val victorias: Int?,
-    val derrotas: Int?,
-    val empates: Int?,
-
-    val partidasComoJugador1: List<PartidaDto>?,
-    val partidasComoJugador2: List<PartidaDto>?,
-    val partidasGanadas: List<PartidaDto>?,
-
-
-    val movimientos: List<MovimientoDto>?
 )
