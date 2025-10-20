0f4aad0e136c369c1255f94cdb338dbdbcf9bc7d
Erick_Estrada <erickestradarosario@gmail.com>
2025-10-20 18:41:10 -0300

    Crea la clase PartidaApiUseCases

---
+++ app/src/main/java/edu/ucne/jugadorestictactoe/domain/useCase/GameUseCases/PartidaUseCases/PartidaApiUseCases.kt
@@ -0,0 +1,8 @@
+package edu.ucne.jugadorestictactoe.domain.useCase.GameUseCases.PartidaUseCases
+
+data class PartidaApiUseCases (
+    val crearPartidaApi: CrearPartidaUseCase,
+    val obtenerPartidaApi: ObtenerPartidaApiUseCase
+)
+
+
