f254b816dbcbebbaa11a99e87eb3a69e591c2d9d
Erick_Estrada <erickestradarosario@gmail.com>
2025-10-20 18:41:17 -0300

    Refactoriza PartidaDto para inicio de partida

--- app/src/main/java/edu/ucne/jugadorestictactoe/data/remote/dto/partida/PartidaDto.kt
+++ app/src/main/java/edu/ucne/jugadorestictactoe/data/remote/dto/partida/PartidaDto.kt
@@ -1,22 +1,8 @@
 package edu.ucne.jugadorestictactoe.data.remote.dto.partida
 
+import com.squareup.moshi.Json
+
 data class PartidaDto (
-    val PartidaId: Int,
-    val Jugador1Id: Int,
-    val Jugador2Id: Int?,
-
-    val EstadoPartida: String?,
-    val GanadorId: Int?,
-    val TurnoJugadorId: Int?,
-    val EstadoTablero: String?,
-
-    val FechaInicio: String?,
-    val FechaFin: String?,
-
-    val Jugador1: String?,
-    val Jugador2: String?,
-    val Ganador: String?,
-    val TurnoJugador: String?,
-
-    val Movimientos: List<MovimientoDto>?
+    val jugador1Id: Int?,
+    val jugador2Id: Int?,
 )
