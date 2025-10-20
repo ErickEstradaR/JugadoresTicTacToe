c8f83e404a29f9dd3b49874b84489a0b5be4466e
Erick_Estrada <erickestradarosario@gmail.com>
2025-10-20 18:40:44 -0300

    Actualiza PartidaApi para simplificar el modelo de datos

--- app/src/main/java/edu/ucne/jugadorestictactoe/domain/model/PartidaApi.kt
+++ app/src/main/java/edu/ucne/jugadorestictactoe/domain/model/PartidaApi.kt
@@ -1,22 +1,6 @@
 package edu.ucne.jugadorestictactoe.domain.model
 
 data class PartidaApi (
-    val PartidaId: Int,
-    val Jugador1Id: Int,
-    val Jugador2Id: Int?,
-
-    val EstadoPartida: String?,
-    val GanadorId: Int?,
-    val TurnoJugadorId: Int?,
-    val EstadoTablero: String?,
-
-    val FechaInicio: String?,
-    val FechaFin: String?,
-
-    val Jugador1: String?,
-    val Jugador2: String?,
-    val Ganador: String?,
-    val TurnoJugador: String?,
-
-    val Movimientos: List<Movimiento>?
+    val jugador1Id: Int?,
+    val jugador2Id: Int?,
 )
