bf3a8c9ee60505ab31038596beac7daf3afae8b8
Erick_Estrada <erickestradarosario@gmail.com>
2025-10-20 18:38:33 -0300

    Refactoriza GameUiState para eliminar campos innecesarios

--- app/src/main/java/edu/ucne/jugadorestictactoe/presentation/tictactoe/GameUiState.kt
+++ app/src/main/java/edu/ucne/jugadorestictactoe/presentation/tictactoe/GameUiState.kt
@@ -1,19 +1,15 @@
 package edu.ucne.jugadorestictactoe.presentation.tictactoe
 
-import edu.ucne.jugadorestictactoe.domain.model.JugadorApi
 
 data class GameUiState(
-    val partidaId: Int? = null,
-
     val board: List<Player?> = List(9) { null },
     val currentPlayer: Player = Player.X,
     val winner: Player? = null,
     val isDraw: Boolean = false,
-    val selectedJugador: JugadorApi? = null,
-
     val gameStarted: Boolean = false,
-
-    val isLoading: Boolean = false,
-    val errorMessage: String? = null
+    val isPosting: Boolean = false,
+    val postError: String? = null,
+    val isLoading : Boolean = false,
+    val loadingError: String? = null,
 )
 
