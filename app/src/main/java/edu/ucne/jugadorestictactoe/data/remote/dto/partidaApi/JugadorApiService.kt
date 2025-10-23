ed3eba4a5e1629adbd5983302860b5d5c3e2292c
ErickEstradaR <erickestradarosario@gmail.com>
2025-10-23 16:05:26 -0400

    Refactor: Actualiza JugadorApiService para CRUD completo

--- app/src/main/java/edu/ucne/jugadorestictactoe/data/remote/dto/partidaApi/JugadorApiService.kt
+++ app/src/main/java/edu/ucne/jugadorestictactoe/data/remote/dto/partidaApi/JugadorApiService.kt
@@ -1,7 +1,10 @@
 package edu.ucne.jugadorestictactoe.data.remote.dto.partidaApi
 
-import edu.ucne.jugadorestictactoe.data.remote.dto.partida.JugadorDto
+import edu.ucne.jugadorestictactoe.data.remote.dto.jugador.JugadorRequest
+import edu.ucne.jugadorestictactoe.data.remote.dto.jugador.JugadorResponse
+import retrofit2.Response
 import retrofit2.http.Body
+import retrofit2.http.DELETE
 import retrofit2.http.GET
 import retrofit2.http.POST
 import retrofit2.http.PUT
@@ -9,12 +12,17 @@ import retrofit2.http.Path
 
 interface JugadorApiService {
     @GET("api/Jugadores")
-    suspend fun getJugadores(): List<JugadorDto>
+    suspend fun getJugadores(): List<JugadorResponse>
 
     @GET("api/Jugadores/{id}")
-    suspend fun getJugador(@Path("id") id: Int): JugadorDto
-
+    suspend fun getJugador(@Path("id") id: Int): JugadorResponse
     @POST("api/Jugadores")
-    suspend fun createJugador(@Body jugador: JugadorDto): JugadorDto
+    suspend fun createJugador(@Body request: JugadorRequest): Response<JugadorResponse>
+
+    @DELETE("api/Jugadores/{id}")
+    suspend fun deleteJugador(@Path("id") id: Int): Response<Unit>
+
+    @PUT("api/Jugadores/{id}")
+    suspend fun updateJugador(@Path("id") id: Int, @Body request: JugadorRequest): Response<Unit>
 
 }
