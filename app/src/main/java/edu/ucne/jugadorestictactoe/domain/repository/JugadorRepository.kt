26765795a58722cca41edf66e7820047243dd331
ErickEstradaR <erickestradarosario@gmail.com>
2025-10-25 19:27:44 -0400

    Actualiza JugadorRepository para soporte offline y API REST

--- app/src/main/java/edu/ucne/jugadorestictactoe/domain/repository/JugadorRepository.kt
+++ app/src/main/java/edu/ucne/jugadorestictactoe/domain/repository/JugadorRepository.kt
@@ -1,12 +1,14 @@
 package edu.ucne.jugadorestictactoe.domain.repository
 
+import edu.ucne.jugadorestictactoe.data.remote.Resource
 import edu.ucne.jugadorestictactoe.domain.model.Jugador
 import kotlinx.coroutines.flow.Flow
 
 interface JugadorRepository {
-    suspend fun save(jugador: Jugador): Boolean
-    suspend fun find(id: Int): Jugador?
-    suspend fun delete(jugador: Jugador)
-    suspend fun getAll(): List<Jugador>
+    suspend fun find(id: String): Jugador?
     fun getAllFlow(): Flow<List<Jugador>>
+    suspend fun postPendingJugadores(): Resource<Unit>
+    suspend fun delete(id: String) : Resource<Unit>
+    suspend fun upsert(jugador: Jugador): Resource<Unit>
+    suspend fun createJugadorLocal(jugador: Jugador): Resource<Jugador>
 }
