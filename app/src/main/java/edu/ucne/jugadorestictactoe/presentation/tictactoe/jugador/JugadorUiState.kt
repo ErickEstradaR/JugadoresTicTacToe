ec345fb0dd5daace1b34bf5322dd3c2af7f0f85d
ErickEstradaR <erickestradarosario@gmail.com>
2025-10-25 19:24:49 -0400

    Refactor: Actualiza JugadorUiState para un manejo de estado simplificado

--- app/src/main/java/edu/ucne/jugadorestictactoe/presentation/tictactoe/jugador/JugadorUiState.kt
+++ app/src/main/java/edu/ucne/jugadorestictactoe/presentation/tictactoe/jugador/JugadorUiState.kt
@@ -1,31 +1,17 @@
 package edu.ucne.jugadorestictactoe.presentation.tictactoe.jugador
 
 import edu.ucne.jugadorestictactoe.data.remote.Resource
+import edu.ucne.jugadorestictactoe.domain.model.Jugador
 import edu.ucne.jugadorestictactoe.domain.model.JugadorApi
 import edu.ucne.jugadorestictactoe.domain.model.Movimiento
 import edu.ucne.jugadorestictactoe.domain.model.PartidaApi
 
 data class JugadorUiState(
-    val JugadorId: Int? = null,
-    val Nombres: String = "",
-    val Email: String = "",
-
-    val FechaCreacion: String? = null,
-    val Victorias: Int? = null,
-    val Derrotas: Int? = null,
-    val Empates: Int? = null,
-
-    val PartidasComoJugador1: List<PartidaApi> = emptyList(),
-    val PartidasComoJugador2: List<PartidaApi> = emptyList(),
-    val PartidasGanadas: List<PartidaApi> = emptyList(),
-
-    val Movimientos: List<Movimiento> = emptyList(),
-    val jugadores: List<JugadorApi> = emptyList(),
-    val errorMessage: String? = null,
-
-    val jugadoresResource: Resource<List<JugadorApi>> = Resource.Loading(),
-    val actionMessage: String? = null,
-    val isSaving: Boolean = false
+    val isLoading: Boolean = false,
+    val jugadores: List<Jugador> = emptyList(),
+    val userMessage: String? = null,
+    val showCreateSheet: Boolean = false,
+    val jugadorDescription: String = ""
 ) {
     companion object {
         fun default() = JugadorUiState()
