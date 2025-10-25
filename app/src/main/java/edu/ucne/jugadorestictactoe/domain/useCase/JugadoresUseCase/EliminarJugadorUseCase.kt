09265772dbc2775e42cc130c18e8f41bc135ff5d
ErickEstradaR <erickestradarosario@gmail.com>
2025-10-25 19:25:30 -0400

    Actualiza `EliminarJugadorUseCase` para inyección de dependencias y cambia el método de eliminación para usar un `id` en lugar de un objeto `Jugador`, devolviendo un `Resource`.

--- app/src/main/java/edu/ucne/jugadorestictactoe/domain/useCase/JugadoresUseCase/EliminarJugadorUseCase.kt
+++ app/src/main/java/edu/ucne/jugadorestictactoe/domain/useCase/JugadoresUseCase/EliminarJugadorUseCase.kt
@@ -1,12 +1,11 @@
 package edu.ucne.jugadorestictactoe.domain.useCase.JugadoresUseCase
 
-import edu.ucne.jugadorestictactoe.domain.model.Jugador
+import edu.ucne.jugadorestictactoe.data.remote.Resource
 import edu.ucne.jugadorestictactoe.domain.repository.JugadorRepository
+import javax.inject.Inject
 
-class EliminarJugadorUseCase(
-    private val repository: JugadorRepository
-) {
-    suspend operator fun invoke(jugador: Jugador) {
-        repository.delete(jugador)
-    }
+class EliminarJugadorUseCase @Inject constructor(
+    private val repo: JugadorRepository
+    ) {
+    suspend operator fun invoke(id: String): Resource<Unit> = repo.delete(id)
 }
