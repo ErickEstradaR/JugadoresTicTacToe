65088e31516a2dbed2524e28e4716b940474501d
ErickEstradaR <erickestradarosario@gmail.com>
2025-10-25 21:30:16 -0400

    Refactor: Cambia nombre de `id` a `jugadorId` en `Jugador`

--- app/src/main/java/edu/ucne/jugadorestictactoe/domain/model/Jugador.kt
+++ app/src/main/java/edu/ucne/jugadorestictactoe/domain/model/Jugador.kt
@@ -3,7 +3,7 @@ package edu.ucne.jugadorestictactoe.domain.model
 import java.util.UUID
 
 data class Jugador(
-    val id: String = UUID.randomUUID().toString(),
+    val jugadorId: String = UUID.randomUUID().toString(),
     val remoteId: Int? = null,
     val nombres: String,
     val email: String,
